80feb8d4a7a73fd8b0886cad2e5201148b4af28a
Osama Yawar <osamayawar@hotmail.com>
2016-03-15 16:19:50 -0400

    Removed constructor from Patient API Controller

--- api/application/controllers/Patient.php
+++ api/application/controllers/Patient.php
@@ -3,19 +3,17 @@ if (!defined('BASEPATH')) exit('No direct script access allowed');
 
 class Patient extends MY_Controller
 {
-	public function __construct() {
-		parent::__construct();
-		if(empty($this->logged_in())) {
-			redirect('/user/login');
-		}
-	}
-
 	public function add() {
 		$data = [];
 		$this->template
 	        ->setViewPath('patient/add')
 	        ->setViewData($data)
 	        ->render();
+	}
+
+	public function getAllPatients() {
+		$query = $this->db->get('user');
+		echo json_encode($query);
 	}
 }
 ?>
